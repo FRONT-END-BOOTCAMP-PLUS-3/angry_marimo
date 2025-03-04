8e0f46634b49d705e21924222852b17ab58db0bb
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2025-03-04 17:02:12 +0900

    :bug: matcher 제외사항에 api 추가

--- middleware.ts
+++ middleware.ts
@@ -23,6 +23,6 @@ export function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|^/pay).*)",
+    "/((?!_next/static|_next/image|api|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|^/pay).*)",
   ],
 }
