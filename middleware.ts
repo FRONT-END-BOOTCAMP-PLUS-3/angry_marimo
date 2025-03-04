fbbc3501ccc639cdd225b61c771dbcdc2424ca22
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2025-03-04 17:00:36 +0900

    :bug: middleware 원상복구..

--- middleware.ts
+++ middleware.ts
@@ -2,34 +2,9 @@ import { NextResponse } from "next/server"
 
 import type { NextRequest } from "next/server"
 
-const allowedOrigins = [process.env.NEXT_WWW_URL, process.env.NEXT_URL]
-
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value
   const { pathname } = req.nextUrl
-
-  const origin = req.headers.get("origin")
-
-  if (pathname.startsWith("/api/") && origin) {
-    const response = NextResponse.next()
-
-    if (allowedOrigins.includes(origin)) {
-      response.headers.set("Access-Control-Allow-Origin", origin)
-    } else {
-      return new Response("Not allowed", { status: 403 })
-    }
-
-    response.headers.set(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS",
-    )
-    response.headers.set(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization",
-    )
-
-    return response
-  }
 
   if (pathname === "/pay/toss/success" || pathname === "/pay/toss/fail") {
     return NextResponse.next()
