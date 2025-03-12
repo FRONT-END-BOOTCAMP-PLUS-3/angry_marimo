f08b32ef96cec7082a0e72e8c03976b023eb77fe
Jyservice781 <gimjuyeong512@gmail.com>
2025-03-12 15:23:16 +0900

    ⚡️ : 수정 완료 #51

--- components/object/object-component.tsx
+++ components/object/object-component.tsx
@@ -22,39 +22,6 @@ export const useObjectComponent = () => {
 
   useWindowEvents(worker)
 
-  // GET 요청 함수
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     if (!marimo) {
-  //       console.log("⚠️ [경고] marimo가 존재하지 않음! API 요청 취소");
-  //       return;
-  //     }
-  //     try {
-    
-  //       const response = await fetch(`/api/objects?marimoId=${marimo.id}`, {
-  //         method: "GET",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //       });
-    
-  //       if (!response.ok) {
-  //         const errorText = await response.text();
-  //         throw new Error(`🚨 API 요청 실패: ${response.status} - ${errorText}`);
-  //       }
-  //       const data = await response.json();
-  //       setTrashItems(data.activeObject);
-
-  //     } catch (error) {
-  //       console.error("❌ [API 요청 중 오류 발생]:", error);
-  //     }
-  //     console.log("[최종 확인용 trashItem]", trashItems)
-  //   };
-    
-  //   fetchData();
-  // }, [marimo])
-
-
   useInterval(() => {
     if (!isWorkerRunning) return
     if (!worker.current) return
